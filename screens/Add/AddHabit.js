a40c3174b75665387bf4c8d07ee9415f9f184712
MohRana <MoharanaOS@outlook.es>
2023-02-03 18:45:29 -0600

    Deleted AddCalendar

--- screens/Add/AddHabit.js
+++ screens/Add/AddHabit.js
@@ -1,18 +1,58 @@
 import * as RN from "react-native";
 import * as React from "react";
 
-export default function AddHabit() {
+import { database } from "../../src/config/fb";
+import { collection, addDoc } from "firebase/firestore";
+
+export default function AddCalendar({ navigation }) {
+  //Values Name, Date, Priority(Options), Description
+  const [newItem, setNewItem] = React.useState({
+    name: "",
+    dwm: "",
+    time: "",
+    icon: "",
+  });
+
+  const onSend = async () => {
+    await addDoc(collection(database, "habitList"), newItem);
+    navigation.goBack();
+  };
+
   return (
-    <RN.View styles={StyleSheet.container}>
-      <RN.Text>Add</RN.Text>
+    <RN.View style={styles.container}>
+      <RN.View>
+        <RN.Text> Add Calendar </RN.Text>
+        <RN.Text> Name </RN.Text>
+        <RN.TextInput
+          style={styles.input}
+          placeholder="Add Name"
+          onChangeText={(text) => setNewItem({ ...newItem, name: text })}
+        />
+        <RN.Text> Date </RN.Text>
+        <RN.TextInput
+          style={styles.input}
+          placeholder="Add Date"
+          onChangeText={(text) => setNewItem({ ...newItem, dwm: text })}
+        />
+        <RN.Text> Priority </RN.Text>
+        <RN.TextInput
+          style={styles.input}
+          placeholder="Add Time"
+          onChangeText={(text) => setNewItem({ ...newItem, time: text })}
+        />
+        <RN.Text> Description </RN.Text>
+        <RN.TextInput
+          style={styles.input}
+          placeholder="Add Icon"
+          onChangeText={(text) => setNewItem({ ...newItem, icon: text })}
+        />
+        <RN.Button title="Add" onPress={onSend} />
+      </RN.View>
     </RN.View>
   );
 }
 
 const styles = RN.StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    alightItems: "center",
-  },
+  container: {},
+  input: {},
 });
--- screens/Add/AddToDo.js
+++ screens/Add/AddToDo.js
@@ -1,18 +1,59 @@
 import * as RN from "react-native";
 import * as React from "react";
 
-export default function AddToDo() {
+//firebase
+import { database } from "../../src/config/fb";
+import { collection, addDoc } from "firebase/firestore";
+
+export default function AddToDo({ navigation }) {
+  //Values Name, Date, Priority(Options), Description
+  const [newItem, setNewItem] = React.useState({
+    name: "",
+    date: "",
+    priority: "",
+    description: "",
+  });
+
+  const onSend = async () => {
+    await addDoc(collection(database, "ToDoList"), newItem);
+    navigation.goBack();
+  };
+
   return (
-    <RN.View styles={StyleSheet.container}>
-      <RN.Text>Add</RN.Text>
+    <RN.View style={styles.container}>
+      <RN.View>
+        <RN.Text> Add Calendar </RN.Text>
+        <RN.Text> Name </RN.Text>
+        <RN.TextInput
+          style={styles.input}
+          placeholder="Add Name"
+          onChangeText={(text) => setNewItem({ ...newItem, name: text })}
+        />
+        <RN.Text> Date </RN.Text>
+        <RN.TextInput
+          style={styles.input}
+          placeholder="Add Date"
+          onChangeText={(text) => setNewItem({ ...newItem, date: text })}
+        />
+        <RN.Text> Priority </RN.Text>
+        <RN.TextInput
+          style={styles.input}
+          placeholder="Add Priority"
+          onChangeText={(text) => setNewItem({ ...newItem, priority: text })}
+        />
+        <RN.Text> Description </RN.Text>
+        <RN.TextInput
+          style={styles.input}
+          placeholder="Add Description"
+          onChangeText={(text) => setNewItem({ ...newItem, description: text })}
+        />
+        <RN.Button title="Add" onPress={onSend} />
+      </RN.View>
     </RN.View>
   );
 }
 
 const styles = RN.StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    alightItems: "center",
-  },
+  container: {},
+  input: {},
 });
